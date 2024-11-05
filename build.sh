099931b8c772fd8c0ddb7669800fb925f2d541a7
Eric Andrechek <eric@wave-rf.com>
2024-11-05 16:42:33 -0500

    build script for caen

--- build.sh
+++ build.sh
@@ -2,7 +2,21 @@
 
 set -x
 
-python3.8 -m venv venv
+# python3.8 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-pyinstaller --onefile --name debugger --add-data "debugger.tcss:." main.py
+
+# read input to ask if clean build --> store clean bool in variable clean
+read -p "Do you want to clean the build folder? (y/n) " clean
+
+# if clean is true, add --clean to the pyinstaller command
+if [ $clean == "y" ]; then
+    pyinstaller --onefile --name debugger --add-data "debugger.tcss:." --clean main.py
+else
+    pyinstaller --onefile --name debugger --add-data "debugger.tcss:." main.py
+fi
+
+cp dist/debugger .
+git add debugger
+git commit -m "Updated debugger"
+git push
